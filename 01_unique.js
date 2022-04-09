b3b26959cba49d0848024fcbec946c1d2bca87e8
ElenaCherpakova <elena.cherpakova@gmail.com>
2022-04-08 20:58:17 -0400

    refactoring

--- 01_unique.js
+++ 01_unique.js
@@ -15,4 +15,4 @@ function isUnique(string) {
 console.log(isUnique('abcdef')) // -> true
 console.log(isUnique('1234567')) // -> true
 console.log(isUnique('abcABC')) // -> true
-console.log(isUnique('abcadef')) // -> false
+console.log(isUnique('abcadefg')) // -> false
