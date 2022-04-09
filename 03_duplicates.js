b39cb8a06f17aa4177adffebf45601674586429f
ElenaCherpakova <elena.cherpakova@gmail.com>
2022-04-08 21:21:59 -0400

    3rd challenge

---
+++ 03_duplicates.js
@@ -0,0 +1,20 @@
+// Write a function that takes a string and returns a new one with all duplicate characters removed.
+
+function removeDupes(str) {
+  const result = [];
+  const map = {};
+  for (let i = 0; i < str.length; i++) {
+    const char = str[i];
+    if (!map[char]) {
+      map[char] = true;
+      result.push(char);
+    }
+
+  }
+  return result.join("");
+}
+
+console.log(removeDupes("abcd")); // -> 'abcd'
+console.log(removeDupes("aabbccdd")); // -> 'abcd'
+console.log(removeDupes("abcddbca")); // -> 'abcd'
+console.log(removeDupes("abababcdcdcd")); // -> 'abcd'
