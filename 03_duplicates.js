2b0a8aef9170f4e18bdec822bec94ca234f5ef69
ElenaCherpakova <elena.cherpakova@gmail.com>
2022-04-08 21:25:11 -0400

    refactoring

--- 03_duplicates.js
+++ 03_duplicates.js
@@ -1,17 +1,20 @@
 // Write a function that takes a string and returns a new one with all duplicate characters removed.
 
 function removeDupes(str) {
-  const result = [];
-  const map = {};
-  for (let i = 0; i < str.length; i++) {
-    const char = str[i];
-    if (!map[char]) {
-      map[char] = true;
-      result.push(char);
-    }
+  // const result = [];
+  // const map = {};
+  // for (let i = 0; i < str.length; i++) {
+  //   const char = str[i];
+  //   if (!map[char]) {
+  //     map[char] = true;
+  //     result.push(char);
+  //   }
 
-  }
-  return result.join("");
+  // }
+  // return result.join("");
+
+  // refactoring
+  return Array.from(new Set(str)).join('')
 }
 
 console.log(removeDupes("abcd")); // -> 'abcd'
