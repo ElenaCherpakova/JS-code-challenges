52feb93a0af2175fedaaaa2a736c9a50979339f7
ElenaCherpakova <elena.cherpakova@gmail.com>
2022-04-10 12:21:43 -0400

    07 Anagrams challenge

---
+++ 07_Anagrams.js
@@ -0,0 +1,14 @@
+// Write a function that checks if all elements in an array are anagrams of each other.
+
+function allAnagrams(array) {
+  const sorted = array.map((str) => str.split("").sort().join(""));
+  for (let i = 1; i < sorted.length; i++) {
+    if (sorted[i] !== sorted[0]) {
+      return false;
+    }
+  }
+  return true;
+}
+
+console.log(allAnagrams(["abcd", "bdac", "cabd"])); // true
+console.log(allAnagrams(["abcd", "bdXc", "cabd"])); // false
