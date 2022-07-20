53ea2a87ebbba62a4bb0b0514e4ac4025f18dada
ElenaCherpakova <elena.cherpakova@gmail.com>
2022-07-20 17:40:30 -0400

    add comments

--- 28_exe.js
+++ 28_exe.js
@@ -1,7 +1,10 @@
 function remove(items, item){
   for (let i=0; i < items.length; i++){
     if (items[i]=== item){
+      //concat using spread operator
       return [...items.slice(0, i), ...items.slice(i+ 1)];
+      // return items.slice(0, i).concat(items.slice(i + 1));
+
     }
   }
 }
