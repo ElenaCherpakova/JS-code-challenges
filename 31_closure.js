9dd509db5fff752fdec97494f5fbf8d14caca3e5
ElenaCherpakova <elena.cherpakova@gmail.com>
2022-08-21 17:12:04 -0400

    practice closures

---
+++ 31_closure.js
@@ -0,0 +1,18 @@
+
+//create a bind function
+function bind(context, func){
+  return function(...args){
+    func.apply(context, args)
+  }
+}
+
+function logPerson() {
+  console.log(`Person: ${this.name}, ${this.age}, ${this.job}`)
+}
+
+const person1 = { name: 'Mike', age: 34, job: 'Frontend' }
+const person2 = { name: 'Elena', age: 33, job: 'Fullstack' }
+
+
+bind(person1, logPerson)()
+bind(person2, logPerson)()
