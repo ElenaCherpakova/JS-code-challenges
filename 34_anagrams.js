d634578f79596d7b73a3415d5a52409603b7d0c2
ElenaCherpakova <elena.cherpakova@gmail.com>
2022-08-24 23:39:06 -0400

    refactor

--- 34_anagrams.js
+++ 34_anagrams.js
@@ -1,32 +1,5 @@
 /*Given 2 strings, write a function to determine if second string is an anagram of the first*/
 
-// function validAnagram(str1, str2) {
-//   if (str1.length !== str2.length) {
-//     return false;
-//   }
-//   let frgChar1 = {};
-//   let frgChar2 = {};
-
-//   for (let char of str1) {
-//     frgChar1[char] = (frgChar1[char] || 0) + 1;
-//   }
-
-//   for (let char of str2) {
-//     frgChar2[char] = (frgChar2[char] || 0) + 1;
-//   }
-//   for (let key in frgChar1) {
-//     if (!key in frgChar2) {
-//       return false;
-//     }
-//     if (frgChar1[key] !== frgChar2[key]) {
-//       return false;
-//     }
-//   }
-//   return true;
-// }
-
-//Refactoring
-
 function validAnagram(first, second) {
   if (first.length !== second.length) {
     return false;
