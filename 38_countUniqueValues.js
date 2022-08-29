1330fb98457de6a92286f3cd559611a824d4b132
ElenaCherpakova <elena.cherpakova@gmail.com>
2022-08-28 20:46:34 -0400

    countUniqueValues

--- 38_countUniqueValues.js
+++ 38_countUniqueValues.js
@@ -1,7 +1,7 @@
 function countUniqueValues(array) {
   let i = 0;
   let j = 1;
-  if(array.length === 0 || array.length === 1) return array.length;
+  if(array.length === 0) return 0;
   while (j < array.length) {
     if (array[i] !== array[j]) {
       i++;
@@ -14,5 +14,5 @@ function countUniqueValues(array) {
 
 console.log(countUniqueValues([1,1,1,1,1,2])) // 2
 console.log(countUniqueValues([1,2,3,4,4,4,7,7,12,12,13])) // 7
-console.log(countUniqueValues([])) // 0
+console.log(countUniqueValues([1])) // 0
 console.log(countUniqueValues([-2,-1,-1,0,1])) // 4
