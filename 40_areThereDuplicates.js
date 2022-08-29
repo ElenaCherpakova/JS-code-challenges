256835f15d8ff362930aa77053be9253faf8dcd0
ElenaCherpakova <elena.cherpakova@gmail.com>
2022-08-29 19:25:41 -0400

    areThereDuplicates

---
+++ 40_areThereDuplicates.js
@@ -0,0 +1,19 @@
+/*Implement a function called, areThereDuplicates which accepts a variable number of arguments, and checks whether there are any duplicates among the arguments passed in.  You can solve this using the frequency counter pattern OR the multiple pointers pattern. */
+
+function areThereDuplicates(...inp) {
+  let obj = {};
+  for (let i = 0; i < inp.length; i++) {
+    if (obj[inp[i]]) {
+      return true;
+    } else {
+      obj[inp[i]] = 1;
+    }
+  }
+  return false;
+}
+
+
+
+console.log(areThereDuplicates(1, 2, 3)) // false
+console.log(areThereDuplicates(1, 2, 2)) // true 
+console.log(areThereDuplicates('a', 'b', 'c', 'a')) // true 
