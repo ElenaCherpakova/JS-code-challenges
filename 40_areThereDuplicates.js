a3e0e130a4c8728c72a074508a8119d237bbe316
ElenaCherpakova <elena.cherpakova@gmail.com>
2022-08-29 19:52:01 -0400

    one liner and multiple pointer solution

--- 40_areThereDuplicates.js
+++ 40_areThereDuplicates.js
@@ -1,18 +1,41 @@
 /*Implement a function called, areThereDuplicates which accepts a variable number of arguments, and checks whether there are any duplicates among the arguments passed in.  You can solve this using the frequency counter pattern OR the multiple pointers pattern. */
 
-function areThereDuplicates(...inp) {
-  let obj = {};
-  for (let i = 0; i < inp.length; i++) {
-    if (obj[inp[i]]) {
-      return true;
-    } else {
-      obj[inp[i]] = 1;
+// function areThereDuplicates(...inp) {
+// let obj = {};
+// for (let i = 0; i < inp.length; i++) {
+//   if (obj[inp[i]]) {
+//     return true;
+//   } else {
+//     obj[inp[i]] = 1;
+//   }
+// }
+// return false;
+// }
+
+function areThereDuplicates(...args) {
+  args.sort((a, b) => {
+    if (typeof a === 'string' && typeof b === 'string') {
+      a = a.charCodeAt(0)
+      b = b.charCodeAt(0)
     }
+    return a - b
+  })
+  let start = 0;
+  let next = 1;
+  while (next < args.length) {
+    if (args[start] === args[next]) {
+      return true
+    }
+    start++
+    next++
   }
-  return false;
+  return false
 }
 
-
+// One Liner Solution
+function areThereDuplicates() {
+  return new Set(arguments).size !== arguments.length;
+}
 
 console.log(areThereDuplicates(1, 2, 3)) // false
 console.log(areThereDuplicates(1, 2, 2)) // true 
