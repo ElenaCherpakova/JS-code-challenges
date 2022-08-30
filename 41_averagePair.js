097ef0c5ce5eaae6d3d806d04106443ab4b37e1d
ElenaCherpakova <elena.cherpakova@gmail.com>
2022-08-30 18:58:41 -0400

    find maxSubArraySum

--- 41_averagePair.js
+++ 41_averagePair.js
@@ -20,7 +20,7 @@ function averagePair(arr, targetAverage) {
 
 
 
-console.log(averagePair([1, 2, 1], 2.5)) // true
+console.log(averagePair([1, 2, 3], 2.5)) // true
 console.log(averagePair([1, 3, 3, 5, 6, 7, 10, 12, 19], 8)) // true
 console.log(averagePair([-1, 0, 3, 4, 5, 6], 4.1)) // false
 console.log(averagePair([], 4)) // false
---
+++ 42_isSubsequence.js
@@ -0,0 +1,22 @@
+/*Write a function called isSubsequence which takes in two strings and checks whether the characters in the first string form a subsequence of the characters in the second string. In other words, the function should check whether the characters in the first string appear somewhere in the second string, without their order changing.*/
+
+
+
+function isSubsequence(strOne, strTwo) {
+  let i = 0;
+  let j = 0;
+  while (i < strOne.length && j < strTwo.length){
+    if (strOne[i] === strTwo[j]){
+      i++;
+    }
+    j++;
+  }
+  return i === strOne.length;
+}
+
+
+
+console.log(isSubsequence('hello world', 'hello world')); // true
+console.log(isSubsequence('sing', 'sting')); // true
+console.log(isSubsequence('abc', 'abracadabra')); // true
+console.log(isSubsequence('abc', 'acb')); // false (order matters)
