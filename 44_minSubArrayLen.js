356ebd506f5653f31b54a2b06934df1e914bb453
ElenaCherpakova <elena.cherpakova@gmail.com>
2022-08-31 17:33:50 -0400

    fix an error

--- 44_minSubArrayLen.js
+++ 44_minSubArrayLen.js
@@ -2,22 +2,34 @@
 
 This function should return the minimal length of a contiguous subarray of which the sum is greater than or equal to the integer passed to the function. If there isn't one, return 0 instead.*/
 
-
 function minSubArrayLen(arr, target) {
-  let minLen = Infinity;
+  let sum = 0;
+  let right = 0;
   let left = 0;
-  let right = 0; 
-  let sum = 0; 
-  for (let i = right; right < arr.length; i++) {
-    sum += arr[right];
-    while (sum >= target) {
-      result = Math.min(result, right - left + 1);
-      sum -= arr[left]; 
+  let minLen = Infinity;
+
+  while (left < arr.length) {
+    // if current window doesn't add up to the given sum then 
+    // move the window to right
+    if (sum < target && right < arr.length) {
+      sum += arr[right];
+      right++;
+    }
+    // if current window adds up to at least the sum given then
+    // we can shrink the window 
+    else if (sum >= target) {
+      minLen = Math.min(minLen, right - left);
+      sum -= arr[left];
       left++;
     }
+    // current total less than required total but we reach the end, need this or else we'll be in an infinite loop 
+    else {
+      break;
+    }
   }
-  return minLen === Infinity ? 0 : result;
+  return minLen === Infinity ? 0 : minLen;
 }
+
 
 console.log(minSubArrayLen([2, 3, 1, 2, 4, 3], 7)) // 2 -> 
 console.log(minSubArrayLen([2, 1, 6, 5, 4], 9)) // 2 -> because [5,4] is the smallest subarray
