af3a30bcef8a910dcd81d767b517c7208f74fcc1
ElenaCherpakova <elena.cherpakova@gmail.com>
2022-09-06 18:41:36 -0400

    feat: add string method toLowerCase()

--- 34_anagrams.js
+++ 34_anagrams.js
@@ -1,19 +1,21 @@
 /*Given 2 strings, write a function to determine if second string is an anagram of the first*/
 
 function validAnagram(first, second) {
-  if (first.length !== second.length) {
+  let str1 = first.toLowerCase();
+  let str2 = second.toLowerCase();
+  if (str1.length !== str2.length) {
     return false;
   }
 
   const lookUp = {};
-  for (let i = 0; i < first.length; i++) {
-    let letter = first[i];
+  for (let i = 0; i < str1.length; i++) {
+    let letter = str1[i];
     //if letter exist => increment by 1
-    lookUp[letter] ? lookUp[letter] += 1 : lookUp[letter] = 1
+    lookUp[letter] ? (lookUp[letter] += 1) : (lookUp[letter] = 1);
   }
-  for (let i = 0; i < second.length; i++) {
-    let letter = second[i];
-    //can't find letter or letter doesn't exist it's not anagram 
+  for (let i = 0; i < str2.length; i++) {
+    let letter = str2[i];
+    //can't find letter or letter doesn't exist it's not anagram
     if (!lookUp[letter]) {
       return false;
     } else {
@@ -23,9 +25,10 @@ function validAnagram(first, second) {
   return true;
 }
 
-console.log(validAnagram(' ', ' '))
-console.log(validAnagram('aaz', 'zaa'))
-console.log(validAnagram('rat', 'cat'))
-console.log(validAnagram('donjohnjava', 'javajohndon'))
+console.log(validAnagram(' ', ' '));
+console.log(validAnagram('aaz', 'zaa'));
+console.log(validAnagram('rat', 'cat'));
+console.log(validAnagram('donjohnjava', 'javajohndon'));
+console.log(validAnagram('Secure', 'Rescue'));
 
 //Time Complexity 0(n)
