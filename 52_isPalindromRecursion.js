d2cf1ea370e9d69113e8ad6894afcd0e44ea4526
ElenaCherpakova <elena.cherpakova@gmail.com>
2022-09-08 18:41:58 -0400

    fix logic

--- 52_isPalindromRecursion.js
+++ 52_isPalindromRecursion.js
@@ -3,12 +3,12 @@
 
 function isPalindrome(str) {
   if (str.length === 0) return true;
-  if (str[0] === str[str.length - 1]) return false;
+  if (str[0] !== str[str.length - 1]) return false;
   return isPalindrome(str.slice(1, -1));
 }
 
 
-console.log(isPalindrome('')) // false
+console.log(isPalindrome('')) // true
 console.log(isPalindrome('foobar')) // false
 console.log(isPalindrome('tacocat')) // true
 console.log(isPalindrome('amanaplanacanalpanama')) // true
