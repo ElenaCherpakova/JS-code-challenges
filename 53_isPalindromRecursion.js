f2b943c02e427bdc3fc6e1bb0a9ea0054541ec56
ElenaCherpakova <elena.cherpakova@gmail.com>
2022-09-08 18:28:11 -0400

    palindrom using recursion

---
+++ 53_isPalindromRecursion.js
@@ -0,0 +1,16 @@
+/*Write a recursive function called isPalindrome which returns true if the string passed to it is a palindrome (reads the same forward and backward). Otherwise it returns false.*/
+
+
+function isPalindrome(str) {
+  if (str.length === 0) return true;
+  if (str[0] !== str[str.length - 1]) return false;
+  return isPalindrome(str.slice(1, -1));
+}
+
+
+console.log(isPalindrome('')) // false
+console.log(isPalindrome('foobar')) // false
+console.log(isPalindrome('tacocat')) // true
+console.log(isPalindrome('amanaplanacanalpanama')) // true
+console.log(isPalindrome('amanaplanacanalpandemonium')) // false
+
