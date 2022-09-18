a7f15c147d92c983107e258b44971f53b459d68b
ElenaCherpakova <elena.cherpakova@gmail.com>
2022-09-18 01:42:05 -0400

    someRecursive function

---
+++ 59_someRecursion.js
@@ -0,0 +1,17 @@
+/*Write a recursive function called someRecursive which accepts an array and a callback. The function returns true if a single value in the array returns true when passed to the callback. Otherwise it returns false.
+
+*/
+const isOdd = val => val % 2 !== 0;
+
+function someRecursive(arr, callback) {
+  if (arr.length === 0) return false;
+  if (callback(arr[0])) return true;
+  return someRecursive(arr.slice(1), callback);
+}
+
+console.log(someRecursive([], isOdd)) // false
+console.log(someRecursive([1,2,3,4], isOdd)) // true
+console.log(someRecursive([4,6,8,9], isOdd)) // true
+console.log(someRecursive([4,6,8], isOdd)) // false
+console.log(someRecursive([4, 6, 8], val => val > 10)); // false
+
