daee36af037ef0a89ed2828a702c70ead99287d1
ElenaCherpakova <elena.cherpakova@gmail.com>
2022-09-18 01:56:24 -0400

    refactor

--- 02_flat.js
+++ 02_flat.js
@@ -4,19 +4,30 @@ function flatten(array) {
   let result = [];
   for (let i = 0; i < array.length; i++) {
     if (Array.isArray(array[i])) {
-      //using recursion
-      const flat = flatten(array[i]);
-      for (let j = 0; j < flat.length; j++) {
-        result.push(flat[j]);
-      }
+      result = result.concat(flatten(array[i]));
     } else {
       result.push(array[i]);
     }
   }
   return result;
+}
+
+  // let result = [];
+  // for (let i = 0; i < array.length; i++) {
+  //   if (Array.isArray(array[i])) {
+  //     //using recursion
+  //     const flat = flatten(array[i]);
+  //     for (let j = 0; j < flat.length; j++) {
+  //       result.push(flat[j]);
+  //     }
+  //   } else {
+  //     result.push(array[i]);
+  //   }
+  // }
+  // return result;
 
   
   // return array.flat(Infinity)
-}
+
 
 console.log(flatten([[1], [[2, 3]], [[[4]]]])); // -> [1, 2, 3, 4]
