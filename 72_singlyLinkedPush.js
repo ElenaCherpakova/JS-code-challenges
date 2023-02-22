9e583e6f5f1454bc935008460091b8b3fe5f7c28
ElenaCherpakova <elena.cherpakova@gmail.com>
2023-02-21 23:38:13 -0500

    practice singlyLinkedList

---
+++ 72_singlyLinkedPush.js
@@ -0,0 +1,29 @@
+class Node {
+  constructor(value) {
+    this.value = value;
+    this.next = null;
+  }
+}
+class SinglyLinkedList {
+  constructor() {
+    this.head = null;
+    this.tail = null;
+    this.length = 0;
+  }
+  push(value) {
+    const newNode = new Node(value);
+    if (!this.head) {
+      this.head = newNode;
+      this.tail = this.head;
+    } else {
+      this.tail.next = newNode;
+      this.tail = newNode;
+    }
+    this.length++;
+  }
+}
+
+const list = new SinglyLinkedList();
+console.log({ list });
+list.push(2);
+console.log({ list });
