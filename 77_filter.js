b30987a5142a2f9964010b41c2cfa7582763e696
ElenaCherpakova <elena.cherpakova@gmail.com>
2023-02-24 13:05:11 -0500

    filter

---
+++ 77_filter.js
@@ -0,0 +1,15 @@
+function filter_list(l) {
+  let result = [];
+  if (!l.length) return result;
+  for (let el of l) {
+    if (typeof el === 'number') {
+      result.push(el);
+    }
+  }
+  return result;
+}
+console.log(filter_list([])); // [1,2]
+
+console.log(filter_list([1, 2, 'a', 'b'])); // [1,2]
+console.log(filter_list([1, 'a', 'b', 0, 15])); // [1,0,15]
+console.log(filter_list([1, 2, 'aasf', '1', '123', 123])); // [1,2,123]
