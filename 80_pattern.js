2825cd779e379bb6da0a28afe391b4496e3a5093
ElenaCherpakova <elena.cherpakova@gmail.com>
2023-03-03 01:12:25 -0500

    repeat pattern

---
+++ 80_pattern.js
@@ -0,0 +1,19 @@
+function pattern(n) {
+  let output = '';
+  // creating the rows of the pattern
+  for (let i = 1; i <= n; i++) {
+    //creating the columns of the pattern and adding quantity of i to the output
+    for (let j = 0; j < i; j++) {
+      output += i;
+    }
+    // adding a new line after each row
+    output += '\n';
+  }
+  return output.trim();
+}
+
+console.log(pattern(1));
+console.log("=======")
+console.log(pattern(2));
+console.log("=======")
+console.log(pattern(5));
