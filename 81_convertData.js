373c9f34442f357dff051cc843fc48b1c6e2c588
ElenaCherpakova <elena.cherpakova@gmail.com>
2023-04-12 20:13:16 -0400

    converData

---
+++ 81_convertData.js
@@ -0,0 +1,15 @@
+const listOfReceivedData = [
+  { type: 'astro', data: 'Saturn Data' },
+  { type: 'bio', data: 'Space Potatoes' },
+  { type: 'physics', data: 'Lagrange Points' },
+  { type: 'bio', data: 'OMG Tardigrades' },
+  { type: 'physics', data: 'Material reflectivity' },
+  { type: 'astro', data: 'Mercury is not the hottest' },
+];
+const convertedData = listOfReceivedData.reduce((acc, cur) => {
+  if (!acc[cur.type]) {
+    acc[cur.type] = [];
+  }
+  acc[cur.type].push(cur.data);
+  return acc;
+}, {});
