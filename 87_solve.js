81d91bd2c040a5e669a4d7dcffbe05e09091287d
ElenaCherpakova <elena.cherpakova@gmail.com>
2023-10-19 18:43:17 -0400

    refactor it

--- 87_solve.js
+++ 87_solve.js
@@ -1,27 +1,21 @@
-//remove duplicates and have this output [4,6,3]
+// function solve(arr) {
+//   let result = [];
+//   let seen = {};
 
-function removeDup(array) {
-  let result = [];
-  for (let i = 0; i < array.length; i++) {
-    if (result.indexOf(array[i]) === -1) {
-      result.push(array[i]);
-    }
-  }
-  return result;
+//   for (let i = arr.length - 1; i >= 0; i--) {
+//     let element = arr[i];
+//     if (!seen.hasOwnProperty(element)) {
+//       result.unshift(element);
+//       seen[element] = true;
+//     }
+//   }
+//   return result;
+// }
+
+function solve(arr){
+   return [...new Set(arr.reverse())].reverse();
+   
 }
 
-function solve(arr) {
-  let result = [];
-  let seen = {};
-
-  for (let i = arr.length - 1; i >= 0; i--) {
-    let element = arr[i];
-    if (!seen.hasOwnProperty(element)) {
-      result.unshift(element);
-      seen[element] = true;
-    }
-  }
-  return result;
-}
 
 console.log(solve([3, 4, 4, 3, 6, 3]));
