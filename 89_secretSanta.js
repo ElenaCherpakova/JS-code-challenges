01c69714eedf0f6f322c4c957b560879394cdb90
ElenaCherpakova <elena.cherpakova@gmail.com>
2023-12-05 22:31:01 -0500

    fix by adding shuffling since the first approach still assigned to themselves

--- 89_secretSanta.js
+++ 89_secretSanta.js
@@ -1,10 +1,18 @@
 const people = ['Alice', 'Bob', 'Carly', 'Dan', 'Ed', 'Ferdinand', 'Ginny'];
-function generateSecretSantaPairs(arr) {
-  let index = Math.floor(Math.random() * arr.length);
 
+// shuffle an array using Fisher-Yates Sorting Algorithm
+function shuffleArr(array) {
+  for (let i = array.length - 1; i > 0; i--) {
+    let j = Math.floor(Math.random() * (i + 1));
+    [array[i], array[j]] = [array[j], array[i]];
+  }
+}
+
+function generateSecretSantaPairs(arr) {
+  shuffleArr(arr);
   let result = {};
   for (let i = 0; i < arr.length; i++) {
-    result[arr[i]] = arr[(index + i + 1) % arr.length];
+    result[arr[i]] = arr[(i + 1) % arr.length];
   }
   return result;
 }
