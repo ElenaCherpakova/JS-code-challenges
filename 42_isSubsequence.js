ecad9fbc7d10171ed95e120546b275494490f5f5
ElenaCherpakova <elena.cherpakova@gmail.com>
2024-01-21 14:49:52 -0500

    add edge case for this case

--- 42_isSubsequence.js
+++ 42_isSubsequence.js
@@ -1,18 +1,21 @@
 /*Write a function called isSubsequence which takes in two strings and checks whether the characters in the first string form a subsequence of the characters in the second string. In other words, the function should check whether the characters in the first string appear somewhere in the second string, without their order changing.*/
 
-function isSubsequence(strOne, strTwo) {
+function isSubsequence(s, t) {
   let i = 0;
   let j = 0;
-  while (i < strOne.length && j < strTwo.length){
-    if (strOne[i] === strTwo[j]){
+  //edge case
+  if(s.length > t.length) return false
+  while (i < s.length && j < t.length){
+    if (s[i] === t[j]){
       i++;
     }
     j++;
   }
-  return i === strOne.length;
+  return i === s.length;
 }
-
-console.log(isSubsequence('hello world', 'hello world')); // true
+// time complexity - O(n)
+// space complexity O(1)
+console.log(isSubsequence('hello world js', 'hello world')); // true
 console.log(isSubsequence('sing', 'sting')); // true
 console.log(isSubsequence('abc', 'abracadabra')); // true
 console.log(isSubsequence('abc', 'acb')); // false (order matters)
