5115af3f47d3e750225a0c204885f2dc27f8ae26
ElenaCherpakova <elena.cherpakova@gmail.com>
2025-01-11 20:56:04 -0500

    using recurrsion to reach certain employee id in nested object

---
+++ 99_orgData_recurrsion.js
@@ -0,0 +1,63 @@
+const organizationData = {
+  headquarters: {
+    finance: {
+      employees: [
+        { id: 1, name: 'Amina Al-Farsi', position: 'Accountant' },
+        { id: 2, name: 'Hiroshi Tanaka', position: 'Financial Analyst' },
+      ],
+      budget: 500000,
+    },
+    hr: {
+      employees: [
+        { id: 3, name: 'Miguel García', position: 'HR Manager' },
+        { id: 4, name: 'Priya Patel', position: 'Recruiter' },
+      ],
+      policies: ['policy1', 'policy2'],
+    },
+  },
+  regional_offices: {
+    office1: {
+      sales: {
+        employees: [
+          { id: 5, name: 'Kwame Mensah', position: 'Sales Executive' },
+        ],
+        revenue: 1000000,
+      },
+      support: {
+        employees: [
+          { id: 6, name: 'Li Wei Chen', position: 'Support Specialist' },
+        ],
+        tickets: 300,
+      },
+    },
+    office2: {
+      development: {
+        employees: [
+          { id: 7, name: 'Emily Smith', position: 'Software Engineer' },
+          { id: 8, name: 'Hank Grey', position: 'Product Manager' },
+        ],
+        projects: ['Project A', 'Project B'],
+      },
+    },
+  },
+};
+
+function searchEmployeeById(data, targetId) {
+  let results = [];
+  for (let key in data) {
+    if (key === 'employees') {
+      data[key].forEach((employee) => {
+        if (employee.id === targetId) {
+          results.push(employee);
+        }
+      });
+    } else if (typeof data[key] === 'object') {
+      results = [...results, ...searchEmployeeById(data[key], targetId)];
+    }
+  }
+  return results;
+}
+const employee1 = searchEmployeeById(organizationData, 5);
+const employee2 = searchEmployeeById(organizationData, 7);
+console.log(employee1);
+console.log(employee2);
